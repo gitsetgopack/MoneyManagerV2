a9dbfba6c0e1831e102e8ed10275fa80ee12734c
abhira0 <82208485+abhira0@users.noreply.github.com>
2024-11-19 21:33:30 +0000

    Remove deprecated files and configurations

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='Track My Dollar',
-    version='4.2',
-    description='An easy to use Telegram Bot to track everyday expenses',
-    author='Anuj, Bhavesh, Jash, Vaibhavi',
-    packages=find_packages()
-)
