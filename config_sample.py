c7a0e5fc4efa46bbc84755a319c7c23d724159d7
abhira0 <82208485+abhira0@users.noreply.github.com>
2024-11-21 06:49:39 +0000

    Update CI workflow to copy config sample and set default environment variables in config_sample.py

--- config_sample.py
+++ config_sample.py
@@ -5,15 +5,15 @@ This module contains configuration settings for the Money Manager application.
 
 import os
 
-MONGO_URI = os.getenv("MONGO_URI", None)
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 
-TOKEN_SECRET_KEY = os.getenv("TOKEN_SECRET_KEY", None)
+TOKEN_SECRET_KEY = os.getenv("TOKEN_SECRET_KEY", "")
 TOKEN_ALGORITHM = os.getenv("TOKEN_ALGORITHM", "HS256")
 
 API_BIND_HOST = os.getenv("API_BIND_HOST", "0.0.0.0")
 API_BIND_PORT = int(os.getenv("API_BIND_PORT", "9999"))
 
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", None)
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "")
 TELEGRAM_BOT_API_BASE_URL = os.getenv(
     "TELEGRAM_BOT_API_BASE_URL", "http://localhost:9999"
 )
