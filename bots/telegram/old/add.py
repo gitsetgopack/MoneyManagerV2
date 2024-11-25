42bfd78c9a4b1b21dbce4a9a0e3d326652dc1d83
Umang Sureshbhai Diyora <87084801+UmangDiyora@users.noreply.github.com>
2024-11-25 09:13:42 +0000

    tmp

---
+++ bots/telegram/old/add.py
@@ -0,0 +1,226 @@
+from telegram_bot_calendar import DetailedTelegramCalendar, LSTEP
+import helper
+import logging
+from telebot import types, telebot
+from datetime import datetime
+from jproperties import Properties
+import requests
+import os
+
+option = {}
+
+configs = Properties()
+
+with open("user.properties", "rb") as read_prop:
+    configs.load(read_prop)
+
+api_token = str(configs.get("api_token").data)
+
+bot = telebot.TeleBot(api_token)
+
+
+if not os.path.exists("receipts"):
+    os.makedirs("receipts")
+
+
+def run(message, bot):
+    helper.read_json()
+    chat_id = message.chat.id
+    option.pop(chat_id, None)  # remove temp choice
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    markup.row_width = 2
+    for c in helper.getSpendCategories():
+        markup.add(c)
+    msg = bot.reply_to(message, "Select Category", reply_markup=markup)
+    bot.register_next_step_handler(msg, post_category_selection, bot)
+
+
+def post_category_selection(message, bot):
+    try:
+        chat_id = message.chat.id
+        selected_category = message.text
+        if selected_category not in helper.getSpendCategories():
+            bot.send_message(
+                chat_id, "Invalid", reply_markup=types.ReplyKeyboardRemove()
+            )
+            raise Exception(
+                'Sorry I don\'t recognise this category "{}"!'.format(selected_category)
+            )
+
+        option[chat_id] = selected_category
+        message = bot.send_message(
+            chat_id,
+            "How much did you spend on {}? \n(Enter numeric values only)".format(
+                str(option[chat_id])
+            ),
+        )
+        bot.register_next_step_handler(
+            message, post_amount_input, bot, selected_category
+        )
+    except Exception as e:
+        logging.exception(str(e))
+        bot.reply_to(message, "Oh no! " + str(e))
+        display_text = ""
+        commands = helper.getCommands()
+        for (
+            c
+        ) in (
+            commands
+        ):  # generate help text out of the commands dictionary defined at the top
+            display_text += "/" + c + ": "
+            display_text += commands[c] + "\n"
+        bot.send_message(chat_id, "Please select a menu option from below:")
+        bot.send_message(chat_id, display_text)
+
+
+def post_amount_input(message, bot, selected_category):
+    try:
+        chat_id = message.chat.id
+        amount_entered = message.text
+        amount_value = helper.validate_entered_amount(amount_entered)  # validate
+        if amount_value == 0:  # cannot be $0 spending
+            raise Exception("Spent amount has to be a non-zero number.")
+
+        helper.validate_transaction_limit(chat_id, amount_value, bot)
+
+        calendar, step = DetailedTelegramCalendar().build()
+        bot.send_message(chat_id, f"Select {LSTEP[step]}", reply_markup=calendar)
+
+        @bot.callback_query_handler(func=DetailedTelegramCalendar.func())
+        def cal(c):
+            result, key, step = DetailedTelegramCalendar().process(c.data)
+
+            if not result and key:
+                bot.edit_message_text(
+                    f"Select {LSTEP[step]}",
+                    c.message.chat.id,
+                    c.message.message_id,
+                    reply_markup=key,
+                )
+            elif result:
+                post_date_input(message, bot, result, amount_value, selected_category)
+                bot.edit_message_text(
+                    f"Date is set: {result}",
+                    c.message.chat.id,
+                    c.message.message_id,
+                )
+
+    except Exception as e:
+        logging.exception(str(e))
+        bot.reply_to(message, "Oh no! " + str(e))
+
+
+def post_date_input(message, bot, date_entered, amount_value, selected_category):
+    try:
+        chat_id = message.chat.id
+        date_of_entry = date_entered.strftime(
+            helper.getDateFormat() + " " + helper.getTimeFormat()
+        )
+        date_str, category_str, amount_str = (
+            str(date_of_entry),
+            str(option[chat_id]),
+            str(amount_value),
+        )
+        helper.write_json(
+            add_user_record(
+                chat_id, "{},{},{}".format(date_str, category_str, amount_str)
+            )
+        )
+
+        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+        markup.row(types.KeyboardButton("Yes, upload receipt"))
+        markup.row(types.KeyboardButton("No, I'm done"))
+        msg = bot.send_message(
+            chat_id,
+            "Do you want to upload a receipt image (if available)?",
+            reply_markup=markup,
+        )
+        bot.register_next_step_handler(
+            msg,
+            handle_receipt_decision,
+            bot,
+            amount_str,
+            category_str,
+            date_str,
+            selected_category,
+        )
+
+    except Exception as e:
+        logging.exception(str(e))
+        bot.reply_to(date_entered, "Oh no. " + str(e))
+
+
+@bot.message_handler(content_types=["photo"])
+def handle_uploaded_receipt(message, bot):
+    try:
+        chat_id = message.chat.id
+        file_id = message.photo[-1].file_id
+        file_info = bot.get_file(file_id)
+        file_url = "https://api.telegram.org/file/bot{}/{}".format(
+            api_token, file_info.file_path
+        )
+
+        # store image
+        file_extension = file_info.file_path.split(".")[-1]
+
+        receipt_file_name = os.path.join(
+            "receipts",
+            f"receipt_{chat_id}_{datetime.now().strftime('%Y%m%d%H%M%S')}.{file_extension}",
+        )
+
+        with open(receipt_file_name, "wb") as file:
+            file.write(requests.get(file_url).content)
+
+        bot.send_message(
+            chat_id, "Receipt uploaded successfully and The expenditure is recorded!"
+        )
+    except Exception as e:
+        logging.exception(str(e))
+        bot.send_message(chat_id, "Error uploading receipt. " + str(e))
+
+
+def handle_receipt_decision(
+    message, bot, amount_str, category_str, date_str, selected_category
+):
+    try:
+        chat_id = message.chat.id
+        decision = message.text.lower()
+
+        if decision == "yes, upload receipt":
+            bot.send_message(chat_id, "Please upload a receipt image.")
+            bot.register_next_step_handler(message, handle_uploaded_receipt, bot)
+        elif decision == "no, i'm done":
+            bot.send_message(chat_id, "The expenditure is recorded!")
+            helper.display_remaining_budget(message, bot, selected_category)
+        else:
+            bot.send_message(
+                chat_id,
+                'Invalid choice. Please select "Yes, upload receipt" or "No, I\'m done".',
+            )
+            markup = types.ReplyKeyboardRemove()
+            msg = bot.send_message(
+                chat_id,
+                "Do you want to upload a receipt image (if available)?",
+                reply_markup=markup,
+            )
+            bot.register_next_step_handler(
+                msg,
+                handle_receipt_decision,
+                bot,
+                amount_str,
+                category_str,
+                date_str,
+                selected_category,
+            )
+    except Exception as e:
+
+        print(f"An exception occurred: {e}")
+
+
+def add_user_record(chat_id, record_to_be_added):
+    user_list = helper.read_json()
+    if str(chat_id) not in user_list:
+        user_list[str(chat_id)] = helper.createNewUserRecord()
+
+    user_list[str(chat_id)]["data"].append(record_to_be_added)
+    return user_list
---
+++ bots/telegram/old/category.py
@@ -0,0 +1,141 @@
+import helper
+import logging
+from telebot import types
+import time
+
+
+# The main funtion of category.py.
+# User can start to manage their categories after calling it
+def run(message, bot):
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    options = helper.getIncomeOrExpense()
+    markup.row_width = 2
+    for c in options.values():
+        markup.add(c)
+    msg = bot.reply_to(message, "Select Income or Expense", reply_markup=markup)
+    bot.register_next_step_handler(msg, post_type_selection, bot)
+
+
+def post_type_selection(message, bot):
+    try:
+        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+        chat_id = message.chat.id
+        selectedType = message.text
+        options1 = helper.getCategoryOptions()
+        markup.row_width = 2
+        for c in options1.values():
+            markup.add(c)
+        msg = bot.reply_to(message, "Select operation", reply_markup=markup)
+        bot.register_next_step_handler(msg, post_operation_selection, bot, selectedType)
+    except Exception as e:
+        # print("hit exception")
+        helper.throw_exception(e, message, bot, logging)
+
+
+# User have three functionality can choose, add a category, delete a category or view the current categories
+def post_operation_selection(message, bot, selectedType):
+    try:
+        chat_id = message.chat.id
+        op = message.text
+        options = helper.getCategoryOptions()
+        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+        # Handle the exception of unknown operation
+        if op not in options.values():
+            bot.send_message(
+                chat_id, "Invalid", reply_markup=types.ReplyKeyboardRemove()
+            )
+            raise Exception('Sorry I don\'t recognise this operation "{}"!'.format(op))
+        if op == options["add"]:
+            msg = bot.reply_to(message, "Please type the new category name")
+            bot.register_next_step_handler(msg, category_add, bot, selectedType)
+        elif op == options["view"]:
+            category_view(message, bot, selectedType)
+        elif op == options["delete"]:
+            markup.row_width = 2
+            for c in helper.getCategories(selectedType):
+                markup.add(c)
+            # Handle the exception to delete the last category
+            if len(helper.getCategories(selectedType)) <= 1:
+                bot.send_message(
+                    chat_id, "Invalid", reply_markup=types.ReplyKeyboardRemove()
+                )
+                raise Exception("Number of categories cannot be zero.")
+            else:
+                msg = bot.reply_to(
+                    message,
+                    "Please choose the category you want to delete",
+                    reply_markup=markup,
+                )
+                bot.register_next_step_handler(msg, category_delete, bot, selectedType)
+    except Exception as e:
+        # print("hit exception")
+        helper.throw_exception(e, message, bot, logging)
+
+
+# Use the funtion to add a new category
+def category_add(message, bot, selectedType):
+    chat_id = message.chat.id
+    category_name = message.text
+    file_name = getFileName(selectedType)
+    with open(file_name, "r") as tf:
+        lines = tf.read().split(",")
+        tf.close()
+    f = open(file_name, "a")
+    if lines == [""]:
+        f.write(category_name)
+    else:
+        f.write("," + category_name)
+    f.close()
+    bot.send_message(chat_id, 'Add category "{}" successfully!'.format(category_name))
+
+
+# Use the function to view all the categories in chat room
+def category_view(message, bot, selectedType):
+    chat_id = message.chat.id
+    file_name = getFileName(selectedType=selectedType)
+    with open(file_name, "r") as tf:
+        lines = tf.read()
+        tf.close()
+    bot.send_message(chat_id, "The categories are:\n{}".format(lines))
+
+
+# Use the funtion to delete a category
+def category_delete(message, bot, selectedType):
+    chat_id = message.chat.id
+    category_name = message.text
+    find_to_delete = False
+    file_name = getFileName(selectedType)
+    with open(file_name, "r") as tf:
+        categories = tf.read().split(",")
+        tf.close()
+    for category in categories:
+        if category == "":
+            categories.remove("")
+        if category == category_name:
+            find_to_delete = True
+            categories.remove(category)
+    # Handle the exception to deleting a not exist category
+    if find_to_delete == False:
+        bot.send_message(
+            chat_id,
+            "Cannot find the category QAQ",
+            reply_markup=types.ReplyKeyboardRemove(),
+        )
+    elif find_to_delete:
+        f = open(file_name, "w")
+        for category in categories:
+            if category == categories[0]:
+                f.write(category)
+            else:
+                f.write("," + category)
+        f.close()
+        bot.send_message(
+            chat_id, 'Delete category "{}" successfully!'.format(category_name)
+        )
+
+
+def getFileName(selectedType):
+    if selectedType == "Income":
+        return "income_categories.txt"
+    else:
+        return "categories.txt"
